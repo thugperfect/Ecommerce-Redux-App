e2e7e37a668fce7b3c0f49f3a6f6e9dd96bf885c
thugperfect <meenamani9444@gmail.com>
2023-09-11 20:17:35 +0530

    getting all comps

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -7,8 +7,14 @@ function Cart({data,price}) {
       {
       data?.length>0
       ? data.map(k=>(
-        <div className='w-full dark:bg-zinc-800 mt-3 mb-3  h-[150px] '>
-          x
+        <div className='w-full dark:bg-zinc-800 mt-3 mb-3  h-[150px] outline outline-1 outline-gray-300 dark:outline-gray-800 rounded-lg flex items-center justify-between'>
+          <img src={k.thumbnail} alt="product-image" className='w-[150px] h-[150px] rounded-l-lg'></img>
+          <div className=' ml-5'>
+            <div className='font-bold text-lg'>{k.title}</div>
+            <div className='w-[300px]'>{k.description}</div>
+          </div>
+          <div className='text-2xl'>Price: {k.price} $</div>
+         <></>
         </div>
       )):<div className='w-full min-h-[40vh] flex justify-center items-center text-2xl'>Nothing in Cart</div>
      }
